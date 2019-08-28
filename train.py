acd1709f2fbbe0e54af14cc042bbbaabe20d5b10
cheukngai <77063219@qq.com>
2019-08-28 19:44:41 +0800

    change padding to 4

--- train.py
+++ train.py
@@ -100,7 +100,7 @@ def main():
         normalize = transforms.Normalize(mean=[0.4914, 0.4822, 0.4465],
                                          std=[0.2023, 0.1994, 0.2010])
         transform_train = transforms.Compose([
-            transforms.RandomCrop(32, padding=2),
+            transforms.RandomCrop(32, padding=4),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             normalize,
